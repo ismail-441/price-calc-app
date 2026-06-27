3480c8f026056d2ab6ba15998c44c0bd7a367511
ismail-441 <mohammed.ismail.441@gmail.com>
2026-06-27 16:40:30 +0530

    fix service worker caching

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "price-calc-v1";
+const CACHE_NAME = "price-calc-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -8,9 +8,19 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
