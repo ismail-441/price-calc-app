a09fc29e84dd9b51ed81a54c8c533750e12dc155
ismail-441 <mohammed.ismail.441@gmail.com>
2026-06-27 14:44:11 +0530

    first version

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "price-calc-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
